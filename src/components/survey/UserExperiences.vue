426825ea2cd00fcc643dc2a8c7820d98f7b04878
Manuel Clairice <m.clairice@rewe-group.at>
2022-08-19 15:49:51 +0200

    implement error handling

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -6,6 +6,8 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey results first.</p>
       <ul v-else>
         <survey-result
           v-for="result in results"
@@ -28,12 +30,14 @@ export default {
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null,
     }
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch('https://vue-http-demo-4010e-default-rtdb.europe-west1.firebasedatabase.app/surveys.json')
       .then((response) => {
         if (response.ok) {
@@ -51,6 +55,11 @@ export default {
        });
       }
       this.results = results;
+    })
+    .catch((error) => {
+      console.log(error);
+      this.isLoading = false;
+      this.error = 'Failed to fetch data - please try again later.'
     });
     },
   },
