0e501b495ddbd139e3b3443f6a556fd9e63a406e
Manuel Clairice <m.clairice@rewe-group.at>
2022-08-19 15:27:12 +0200

    add loading spinner

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -26,11 +27,13 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vue-http-demo-4010e-default-rtdb.europe-west1.firebasedatabase.app/surveys.json')
       .then((response) => {
         if (response.ok) {
@@ -38,6 +41,7 @@ export default {
         }
       }
     ).then((data) => {
+      this.isLoading = false;
       const results = [];
       for (const id in data) {
         results.push({ 
